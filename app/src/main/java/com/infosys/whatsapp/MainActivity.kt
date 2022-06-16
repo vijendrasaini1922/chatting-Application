f38a035f3bcb7ae6edcf13d85cf8d0d298f81b28
vijendrasaini1922 <virajsaini0@gmail.com>
2022-06-16 17:56:07 +0530

    frontend of login and signup page done

--- app/src/main/java/com/infosys/whatsapp/MainActivity.kt
+++ app/src/main/java/com/infosys/whatsapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.infosys.whatsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.infosys.whatsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
--- app/src/main/java/com/infosys/whatsapp/login.kt
+++ app/src/main/java/com/infosys/whatsapp/login.kt
@@ -1,11 +1,41 @@
 package com.infosys.whatsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.databinding.DataBindingUtil
+import com.infosys.whatsapp.databinding.ActivityLoginBinding
 
 class login : AppCompatActivity() {
+
+    private lateinit var login_binding: ActivityLoginBinding
+
+    private lateinit var editEmail:EditText
+    private lateinit var editPassword:EditText
+    private lateinit var loginBtn:Button
+    private lateinit var signupBtn:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        //setContentView(R.layout.activity_login)
+        login_binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        login_binding.apply {
+            editEmail = login_binding.email
+            editPassword = login_binding.password
+            this@login.loginBtn = login_binding.loginBtn
+            this@login.signupBtn = login_binding.signupBtn
+        }
+
+        signupBtn.setOnClickListener { jmpToSignup() }
+
     }
+
+    private fun jmpToSignup(){
+            val intent = Intent(this, signup::class.java)
+            startActivity(intent)
+    }
+
 }
