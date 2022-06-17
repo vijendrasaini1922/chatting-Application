3750776bbaf1d770d445cabe19e2ccc2178d7d2d
vijendrasaini1922 <virajsaini0@gmail.com>
2022-06-17 09:09:39 +0530

    Created Authentication for user using firebase

--- app/src/main/java/com/infosys/whatsapp/login.kt
+++ app/src/main/java/com/infosys/whatsapp/login.kt
@@ -5,22 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
 import com.infosys.whatsapp.databinding.ActivityLoginBinding
 
 class login : AppCompatActivity() {
 
     private lateinit var login_binding: ActivityLoginBinding
 
-    private lateinit var editEmail:EditText
-    private lateinit var editPassword:EditText
-    private lateinit var loginBtn:Button
-    private lateinit var signupBtn:Button
+    private lateinit var editEmail: EditText
+    private lateinit var editPassword: EditText
+    private lateinit var loginBtn: Button
+    private lateinit var signupBtn: Button
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+
         login_binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        mAuth = FirebaseAuth.getInstance()
 
         login_binding.apply {
             editEmail = login_binding.email
@@ -31,11 +38,34 @@ class login : AppCompatActivity() {
 
         signupBtn.setOnClickListener { jmpToSignup() }
 
+        loginBtn.setOnClickListener {
+            val email = editEmail.text.toString()
+            val password = editPassword.text.toString()
+
+            loginToPage(email, password)
+        }
+
     }
 
-    private fun jmpToSignup(){
-            val intent = Intent(this, signup::class.java)
-            startActivity(intent)
+    private fun jmpToSignup() {
+        val intent = Intent(this, signup::class.java)
+        startActivity(intent)
+    }
+
+    private fun loginToPage(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val intent = Intent(this@login, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@login, "Error", Toast.LENGTH_SHORT).show()
+                }
+            }
+
     }
 
 }
--- app/src/main/java/com/infosys/whatsapp/signup.kt
+++ app/src/main/java/com/infosys/whatsapp/signup.kt
@@ -1,11 +1,63 @@
 package com.infosys.whatsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.infosys.whatsapp.databinding.ActivitySignupBinding
 
 class signup : AppCompatActivity() {
+
+    private lateinit var signupBinding : ActivitySignupBinding
+
+    private lateinit var editEmail: EditText
+    private lateinit var editPassword: EditText
+    private lateinit var editName: EditText
+    private lateinit var signupBtn: Button
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        //setContentView(R.layout.activity_signup)
+
+        supportActionBar?.hide()
+        signupBinding = DataBindingUtil.setContentView(this, R.layout.activity_signup)
+        mAuth = FirebaseAuth.getInstance()
+
+        signupBinding.apply {
+            editEmail = signupBinding.email
+            editPassword = signupBinding.password
+            this@signup.signupBtn = signupBinding.signupBtn
+            editName = signupBinding.userName
+        }
+
+        signupBtn.setOnClickListener {
+            val email = editEmail.text.toString()
+            val password = editPassword.text.toString()
+            //val name  = editName.text.toString()
+            signUp(email, password)
+        }
+
+    }
+
+    private fun signUp(email: String, password: String ) {
+        // logic of creating user
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // code for jumping to home activity
+                    val intent = Intent(this@signup, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@signup, "Some error occurred", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
